1c537a5634615ef4ebe1794727e9a15390564f9b
prashantsangal1 <69078671+prashantsangal1@users.noreply.github.com>
2020-09-04 22:47:39 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# collison-test
-Algorithm to calculate the distortion of a car upon hitting a wall with random speed and random mass.
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,35 @@
+var wall, car;
+var speed,height;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(55,90);
+  weight = random(400,1500);
+  
+  car = createSprite(50,200,50,25);
+  wall = createSprite(1500,200,60,200);
+  
+  car.velocityX = speed;
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  if(wall.x-car.x < car.width+wall.width/2){
+    car.velocityX = 0;
+    var deformation = 0.5 * weight * speed * speed/22500;
+    if(deformation > 180)
+    {
+      car.shapeColor = color(255,0,0);
+    }
+    if(deformation < 180 && deformation > 100)
+    {
+      car.shapeColor = color(230,230,0);
+    }
+    if(deformation < 100)
+    {
+      car.shapeColor = color(0,255,0);
+    }
+  }
+}
